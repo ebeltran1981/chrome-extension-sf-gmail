08c0d6032c45e86eb3351eab9f38ce99dd002095
Edwin Beltran <edwin.beltran@atlanticbt.com>
2017-05-08 17:34:29 -0400

    events for ext

--- src/js/app/index.ts
+++ src/js/app/index.ts
@@ -1,3 +1,25 @@
 module AtlanticBTApp {
-    
+    class Bootstrapper {
+        private $amount = $("amount");
+        private $total = $("total");
+
+        constructor(){
+            chrome.storage.sync.get("total", (items) => {
+                let newTotal = 0;
+                if(items.total){
+                    newTotal += parseInt(items.total);
+                }
+                
+                let amount = this.$amount.val();
+                if(amount) {
+                    newTotal += parseInt(amount);
+                }
+
+                chrome.storage.sync.set({"total": newTotal});
+            });
+        }
+    }
+    $(document).ready(() => {
+        
+    });
 }
