ccc8c3d56cabc3aba543cb6dc4269fe2890a31a7
Edwin Beltran <ebeltran1981@gmail.com>
2017-05-16 08:22:16 -0400

    changed on redirectUri

--- src/ts/services/salesforce.services.ts
+++ src/ts/services/salesforce.services.ts
@@ -9,7 +9,8 @@ namespace AtlanticBTApp {
         private _sfconn: any;
         private _ck: string = "3MVG9i1HRpGLXp.qijeggn1OC_wgtTx0cAhvRPveCdd69JJJWao_fGV4l.DDN.kd16oZ3ZTawpHW4I3Y7RIyK";
         private _cs: string = "8052009589975949097";
-        private _redirectUri: string = "https://login.salesforce.com/services/oauth2/callback";
+        private _redirectUri: string = "https://mail.google.com";
+        // private _redirectUri: string = "https://login.salesforce.com/services/oauth2/callback";
 
         constructor() {
             // this._force = new sf.Connection();
@@ -21,18 +22,20 @@ namespace AtlanticBTApp {
                 debugger;
             });
 
-            sf.browser.login({
+            sf.browser.init({
                 clientId: this._ck,
                 redirectUri: this._redirectUri
             });
 
-            // sf.browser.init({
-            //     clientId: this._ck,
-            //     redirectUri: this._redirectUri
-            // });
+            sf.browser.login();
 
             sf.browser.on("connect", (conn: any) => {
                 debugger;
+                conn.query("SELECT Id, Name FROM Account", (err, res) => {
+                    debugger;
+                    if (err) { return console.error(err); }
+                    console.log(res);
+                });
             });
         }
     }
