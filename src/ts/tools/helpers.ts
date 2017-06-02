1ceb720d9c578c67c0db7a5b887aed9670a8589d
Edwin Beltran <edwin.beltran@atlanticbt.com>
2017-06-02 12:00:27 -0400

    change the way to find email on the span elements to target the email attribute

--- src/ts/tools/helpers.ts
+++ src/ts/tools/helpers.ts
@@ -49,147 +49,134 @@ namespace AtlanticBTApp {
             const sforce = this._sforce;
             const composeEl = this._composeEl;
             const composes = gmail.dom.composes();
-            if (sforce.isLoggedIn()) {
-                $.each(composes, (idx: number, item: GmailDomCompose) => {
-                    const form = item.$el.find("form");
 
-                    if (composeEl.hasToolbar(form)) {
-                        return;
+            $.each(composes, (idx: number, item: GmailDomCompose) => {
+                const form = item.$el.find("form");
+
+                if (composeEl.hasToolbar(form)) {
+                    return;
+                }
+
+                const toolbar = composeEl.add_toolbar(form);
+
+                // Bcc Salesforce
+                const chkDiv = composeEl.checkbox("bcc_salesforce", "Bcc Salesforce", "checkbox", false);
+                toolbar.append(chkDiv);
+                const chk = chkDiv.find("input[type=checkbox]");
+
+                chk.on("change", (e) => {
+                    if ((e.currentTarget as HTMLInputElement).checked) {
+                        if (!sforce.isLoggedIn()) {
+                            const notification = {
+                                type: "basic",
+                                title: "WARNING",
+                                message: "You are not logged in Salesforce"
+                            };
+                            const message = new ChromeMessage(ChromeMessageKeys.CreateNotification, notification);
+                            chrome.runtime.sendMessage(message);
+                        }
                     }
-
-                    const toolbar = composeEl.add_toolbar(form);
-
-                    // Bcc Salesforce
-                    const chkDiv = composeEl.checkbox("bcc_salesforce", "Bcc Salesforce", "checkbox", false);
-                    toolbar.append(chkDiv);
-                    const chk = chkDiv.find("input[type=checkbox]");
-
-                    chk.on("change", (e) => {
-                        if ((e.currentTarget as HTMLInputElement).checked) {
-                            if (!sforce.isLoggedIn()) {
-                                const notification = {
-                                    type: "basic",
-                                    title: "WARNING",
-                                    message: "You are not logged in Salesforce"
-                                };
-                                const message = new ChromeMessage(ChromeMessageKeys.CreateNotification, notification);
-                                chrome.runtime.sendMessage(message);
-                            }
-                        }
-                    });
-
-                    const sendBtn = item.dom("send_button");
-                    sendBtn.on("click", (e) => {
-                        const bccSalesforce = chk.is(":checked");
-                        if (bccSalesforce) {
-                            const $toList: JQuery = item.to() as any;
-                            const toList: string[] = [];
-                            $.each($toList.siblings("div.vR").children("span.vN.vP"), (elIdx, elem) => {
-                                _.forEach(elem.attributes, (attr) => {
-                                    if (attr.nodeName === "email") {
-                                        toList.push(attr.nodeValue);
-                                    }
-                                });
-                            });
-
-                            const $ccList: JQuery = item.cc() as any;
-                            const ccList: string[] = [];
-                            $.each($ccList.siblings("div").children("span"), (elIdx, elem) => {
-                                _.forEach(elem.attributes, (attr) => {
-                                    if (attr.nodeName === "email") {
-                                        ccList.push(attr.nodeValue);
-                                    }
-                                });
-                            });
-
-                            const $bccList: JQuery = item.bcc() as any;
-                            const bccList: string[] = [];
-                            $.each($bccList.siblings("div").children("span"), (elIdx, elem) => {
-                                _.forEach(elem.attributes, (attr) => {
-                                    if (attr.nodeName === "email") {
-                                        bccList.push(attr.nodeValue);
-                                    }
-                                });
-                            });
-
-                            const bodyContainer = $(document.createElement("div"));
-                            bodyContainer.html(item.body());
-                            bodyContainer.find(".gmail_signature").remove();
-
-                            let body = _.replace(bodyContainer.html(), /<br>/gi, "\n\n");
-                            body = _.replace(body, /<br\/>/gi, "\n\n");
-                            body = _.replace(body, /<\/p>/gi, "</p>\n\n");
-                            body = _.replace(body, /<\/div>/gi, "</div>\n\n");
-                            body = _.replace(body, /<\/h1>/gi, "</h1>\n\n");
-                            body = _.replace(body, /<\/h2>/gi, "</h2>\n\n");
-                            body = _.replace(body, /<\/h3>/gi, "</h3>\n\n");
-                            body = _.replace(body, /<\/h4>/gi, "</h4>\n\n");
-                            body = _.replace(body, /<\/h5>/gi, "</h5>\n\n");
-                            body = _.replace(body, /<\/h6>/gi, "</h6>\n\n");
-
-                            bodyContainer.html(body);
-
-                            body = bodyContainer.text();
-
-                            body = _.truncate(body, {
-                                length: 32000 - 58, // doubled the limit due to the omission text.
-                                omission: "[-----MESSAGE TRUNCATED-----]"
-                            });
-
-                            const subject = item.subject();
-
-                            body = EmailTemplates.SforceTask
-                                .replace("{{from}}", this._sforce.currentUser.email)
-                                .replace("{{toList}}", _.join(toList, ", "))
-                                .replace("{{ccList}}", _.join(ccList, ", "))
-                                .replace("{{bccList}}", _.join(bccList, ", "))
-                                .replace("{{subject}}", subject)
-                                .replace("{{body}}", body);
-
-                            let contacts: ISforceContactModel[] = [];
-
-                            _.forEach(toList, (toEmail) => {
-                                sforce.connection
-                                    .sobject("Contact")
-                                    .select("Id, AccountId")
-                                    .where({
-                                        Email: toEmail
-                                    })
-                                    .execute((errC, resC: ISforceContactModel[]) => {
-                                        if (errC) {
-                                            return console.error(`Error finding Contact: ${toEmail}`);
-                                        }
-                                        _.forEach(resC, (contact) => {
-                                            contacts.push(contact);
-                                        });
-
-                                        // Remove possible duplicates on contacts
-                                        contacts = _.uniqBy(contacts, "Id");
-
-                                        _.forEach(contacts, (c) => {
-                                            const task = {
-                                                OwnerId: sforce.currentUser.user_id,
-                                                Subject: subject,
-                                                Description: body,
-                                                TaskSubtype: "Email",
-                                                WhoId: c.Id,
-                                                WhatId: c.AccountId
-                                            };
-                                            sforce.connection
-                                                .sobject("Task")
-                                                .create(task, (errT, resT) => {
-                                                    if (errT || !resT.success) {
-                                                        return console.error(errT, resT);
-                                                    }
-                                                    console.log("Task created");
-                                                });
-                                        });
-                                    });
-                            });
-                        }
-                    });
                 });
-            }
+
+                const sendBtn = item.dom("send_button");
+                sendBtn.on("click", (e) => {
+                    const bccSalesforce = chk.is(":checked");
+                    if (bccSalesforce) {
+                        const $toList: JQuery = item.to() as any;
+                        const toList: string[] = [];
+                        $.map($toList.siblings().children("span[email]"), (element) => {
+                            toList.push($(element).attr("email"));
+                        });
+
+                        const $ccList: JQuery = item.cc() as any;
+                        const ccList: string[] = [];
+                        $.map($ccList.siblings().children("span[email]"), (element) => {
+                            ccList.push($(element).attr("email"));
+                        });
+
+                        const $bccList: JQuery = item.bcc() as any;
+                        const bccList: string[] = [];
+                        $.map($bccList.siblings().children("span[email]"), (element) => {
+                            bccList.push($(element).attr("email"));
+                        });
+
+                        const bodyContainer = $(document.createElement("div"));
+                        bodyContainer.html(item.body());
+                        bodyContainer.find(".gmail_signature").remove();
+
+                        let body = _.replace(bodyContainer.html(), /<br>/gi, "\n\n");
+                        body = _.replace(body, /<br\/>/gi, "\n\n");
+                        body = _.replace(body, /<\/p>/gi, "</p>\n\n");
+                        body = _.replace(body, /<\/div>/gi, "</div>\n\n");
+                        body = _.replace(body, /<\/h1>/gi, "</h1>\n\n");
+                        body = _.replace(body, /<\/h2>/gi, "</h2>\n\n");
+                        body = _.replace(body, /<\/h3>/gi, "</h3>\n\n");
+                        body = _.replace(body, /<\/h4>/gi, "</h4>\n\n");
+                        body = _.replace(body, /<\/h5>/gi, "</h5>\n\n");
+                        body = _.replace(body, /<\/h6>/gi, "</h6>\n\n");
+
+                        bodyContainer.html(body);
+
+                        body = bodyContainer.text();
+
+                        body = _.truncate(body, {
+                            length: 32000 - 58, // doubled the limit due to the omission text.
+                            omission: "[-----MESSAGE TRUNCATED-----]"
+                        });
+
+                        const subject = item.subject();
+
+                        body = EmailTemplates.SforceTask
+                            .replace("{{from}}", this._sforce.currentUser.email)
+                            .replace("{{toList}}", _.join(toList, ", "))
+                            .replace("{{ccList}}", _.join(ccList, ", "))
+                            .replace("{{bccList}}", _.join(bccList, ", "))
+                            .replace("{{subject}}", subject)
+                            .replace("{{body}}", body);
+
+                        let contacts: ISforceContactModel[] = [];
+
+                        _.forEach(toList, (toEmail) => {
+                            sforce.connection
+                                .sobject("Contact")
+                                .select("Id, AccountId")
+                                .where({
+                                    Email: toEmail
+                                })
+                                .execute((errC, resC: ISforceContactModel[]) => {
+                                    if (errC) {
+                                        return console.error(`Error finding Contact: ${toEmail}`);
+                                    }
+                                    _.forEach(resC, (contact) => {
+                                        contacts.push(contact);
+                                    });
+
+                                    // Remove possible duplicates on contacts
+                                    contacts = _.uniqBy(contacts, "Id");
+
+                                    _.forEach(contacts, (c) => {
+                                        const task = {
+                                            OwnerId: sforce.currentUser.user_id,
+                                            Subject: subject,
+                                            Description: body,
+                                            TaskSubtype: "Email",
+                                            WhoId: c.Id,
+                                            WhatId: c.AccountId
+                                        };
+                                        sforce.connection
+                                            .sobject("Task")
+                                            .create(task, (errT, resT) => {
+                                                if (errT || !resT.success) {
+                                                    return console.error(errT, resT);
+                                                }
+                                                console.log("Task created");
+                                            });
+                                    });
+                                });
+                        });
+                    }
+                });
+            });
         }
     }
 }
